07f8e8013f919647dad0cd2d183b2469e380fa1f
meisZWFLZ <atcurtis08@gmail.com>
2024-06-29 18:43:27 -0700

    fix: :bug: fix conversion compile error

--- src/subsytems/handler.cpp
+++ src/subsytems/handler.cpp
@@ -4,7 +4,7 @@
 
 SubsystemHandler::SubsystemHandler()
   : m_lastUsedId(0), m_subsystems {}, m_task {[this] {
-      unsigned int now = pros::millis();
+      uint32_t now = pros::millis();
       while (true) {
         for (auto sub : m_subsystems) sub.second->update();
         pros::c::task_delay_until(&now, 10);
