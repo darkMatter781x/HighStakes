5d75734364d7f1bf5fd4af3f0b696fe3ff879398
meisZWFLZ <atcurtis08@gmail.com>
2024-08-11 22:21:35 -0700

    feat: :loud_sound: add error log for subsystem destruction

--- src/subsytems/subsytem.cpp
+++ src/subsytems/subsytem.cpp
@@ -2,4 +2,8 @@
 
 Subsystem::Subsystem() : m_id(SubsystemHandler::get()->addSubsystem(this)) {}
 
-Subsystem::~Subsystem() { SubsystemHandler::get()->removeSubsystem(m_id); }
+Subsystem::~Subsystem() {
+  // this should never be run
+  printf("destroying subsystem with id of '%i'!\n", m_id);
+  SubsystemHandler::get()->removeSubsystem(m_id);
+}
